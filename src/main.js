4169887ca286fb7f1275cb83c9fe2e6b5793f4a6
iktnb <iknowthatinobody@gmail.com>
2024-04-23 22:29:04 +0300

    install pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
+import { createPinia } from 'pinia'
 import App from './App.vue'     //Our .vue startup file
 import './static/global.css'    //Import css files as modules
 
-//Will mount the vue app inside a HTML element which id equals to "app" (div into templetes/index.html file)
+
+const pinia = createPinia()
+
+
 createApp(App).mount('#app');
